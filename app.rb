67d51c7634188d1020825bd10422979eea8798d7
Micah Bales <micahbales@gmail.com>
2017-07-07 15:05:29 -0400

    display image with each tweet

--- app.rb
+++ app.rb
@@ -16,13 +16,29 @@ get '/' do
   "#dctech
   filter:images
   -filter:retweets",
-  result_type: "recent")
+  # return recent tweets
+  result_type: "recent",
+  # get extended tweets that include all the information we need
+  # including media entities (images)
+  tweet_mode: "extended")
   .take(3)
   .collect do |tweet|
-    "#{tweet.user.screen_name}: #{tweet.text} <br />
-    Created At: #{tweet.created_at} || Retweets: #{tweet.retweet_count}"
+    {
+      :tweet_body => "
+      <div style='width: 700px;'>
+        @#{tweet.user.screen_name}: #{tweet.to_h[:full_text]} <br />
+        Created At: #{tweet.created_at} || Retweets: #{tweet.retweet_count} <br />
+        <img style='width: 500px;' src='#{tweet.media[0].media_url_https.to_s}' />
+      </div>
+      "
+    }
+
   end
 
   erb :index
 
 end
+
+# require 'active_model_serializers'
+# tweet.as_json
+# tweet.entities.media.media_url_https
